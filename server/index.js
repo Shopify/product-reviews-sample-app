db14157611a51d7c6f0ec98ec5743686c85eed86
Megan Majewski <megan.majewski@shopify.com>
2022-03-01 09:44:49 -0500

    replace deprecated babel packages

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
-require('@babel/register')({
-  presets: ['@babel/preset-env'],
-  ignore: ['node_modules']
+require("@babel/register")({
+  presets: ["@babel/preset-env"],
 });
 
 // Import the rest of our application.
-module.exports = require('./server.js');
+module.exports = require("./server.js");
